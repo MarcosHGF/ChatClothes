14fc46a61599a7e7d314706004d1c2d7c3d255fb
Marcos <126510019+MarcosHGF@users.noreply.github.com>
2025-04-08 19:39:17 -0300

    Update database.py

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,10 +1,11 @@
 import sqlite3
 
 def conectar_bd():
-    """Cria o banco de dados e a tabela se não existirem."""
+    """Cria o banco de dados e as tabelas se não existirem."""
     conn = sqlite3.connect("loja.db")
     cursor = conn.cursor()
 
+    # Tabela de produtos
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,8 +13,33 @@ def conectar_bd():
             cor TEXT NOT NULL,
             tamanho TEXT NOT NULL,
             preco REAL NOT NULL,
-            imagem TEXT NOT NULL,
+            imagem TEXT,
             quantidade INTEGER NOT NULL
+        )
+    ''')
+
+    # Tabela de clientes
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS clientes (
+            celular TEXT PRIMARY KEY,
+            nome TEXT NOT NULL,
+            caracteristicas_pesquisadas TEXT
+        )
+    ''')
+
+    # Tabela Vendas
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS vendas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo_produto TEXT NOT NULL,
+            cor_produto TEXT NOT NULL,
+            tamanho_produto TEXT NOT NULL,
+            quantidade_vendida INTEGER NOT NULL,
+            data_venda TEXT NOT NULL,
+            preco_unitario REAL NOT NULL,
+            valor_total REAL NOT NULL,
+            FOREIGN KEY (celular_cliente) REFERENCES clientes(celular)
         )
     ''')
 
